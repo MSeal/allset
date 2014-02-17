e3910d862869c3f1489c845c25f121935ab2c2a6
MSeal <mseal007@gmail.com>
2014-02-17 13:23:48 -0800

    Added base framework for allset

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='allset',
+    version="1.0.0",
+    description='Generates dynamic bindings for module imports',
+    author='Matthew Seal',
+    author_email="mseal@opengov.us",
+    packages=['allset'],
+)
