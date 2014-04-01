3ac827eea0de048eda4bcb3ae2a66b326a147599
MSeal <mseal007@gmail.com>
2014-04-01 14:47:40 -0700

    Fixed bug that allows removal of chdir

--- allset/_allset.py
+++ allset/_allset.py
@@ -1,4 +1,4 @@
-import os, pkgutil, sys
+import os, pkgutil, sys, glob
 
 def _get_module_directory(namespace):
     return os.path.dirname(namespace['__file__'])
@@ -8,7 +8,7 @@ def _get_module_name(namespace):
     #return os.path.basename(_get_module_directory(namespace))
 
 def _get_submodules(namespace):
-    return list(pkgutil.iter_modules([_get_module_name(namespace)]))
+    return list(pkgutil.iter_modules([_get_module_directory(namespace)]))
 
 def _assign_modules_names(filter_mods, namespace, import_modules):
     namespace['__all__'] = filter_mods([module[1] for module in import_modules])
@@ -26,21 +26,8 @@ def _load_modules(namespace, import_modules):
             module = module_loader.find_module(submodule).load_module(submodule)
         namespace[name] = module
 
-def _run_over_modules(namespace, runner):
-    old_dir = os.getcwd()
-    try:
-        modules = _get_submodules(namespace)
-        if not modules:
-            # This is a hack to trick pkgutil into loading modules from site-packages
-            os.chdir(_get_module_directory(namespace))
-            modules = _get_submodules({'__name__': '.'})
-        return runner(namespace, modules)
-    finally:
-        os.chdir(old_dir)
-
 def set_all_submodules(namespace, filter_mods=None):
-    all_assigner = lambda ns, mods: _assign_modules_names(filter_mods or _pass_through, ns, mods)
-    return _run_over_modules(namespace, all_assigner)
+    return _assign_modules_names(filter_mods or _pass_through, namespace, _get_submodules(namespace))
 
 def bind_all_submodules(namespace):
-    _run_over_modules(namespace, _load_modules)
+    _load_modules(namespace, _get_submodules(namespace))
